1999a248c88c8600cafcdd54774c1f3e6601cd19
Sk Shad <hasibul15-7748@diu.edu.bd>
2025-04-07 04:21:47 +0600

    url change

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://jobportal-rvte.onrender.com/', //'http://localhost:5173'
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
